832004c3cf0daa99a0a425233f027acb21c329be
Parth <parthwani22@gmail.com>
2025-11-24 07:51:12 -0500

    Added other operations

--- DebugCode/debug_code.js
+++ DebugCode/debug_code.js
@@ -3,8 +3,12 @@ function performOperation() {
     let num2 = parseInt(document.getElementById('input2').value);
     
     if(!isNaN(num1) && !isNaN(num2)) {
-        let result = multiply(num1, num2);
-        displayResult(result);
+        let resultMul = multiply(num1, num2);
+        let resultAdd = add(num1, num2);
+        let resultSub = subtract(num1, num2);
+        let resultDiv = division(num1, num2);
+
+        displayResult(resultMul, resultAdd, resultSub, resultDiv);
     } else {
         displayResult('Please enter valid numbers');
     };
@@ -15,7 +19,26 @@ function multiply(a, b) {
     return a * b;
 };
 
-function displayResult(result) {
+function add(a, b) {
+    debugger;
+    return a + b;
+};
+
+function subtract(a, b) {
+    debugger;
+    if(a > b) {
+        return a - b;
+    } else {
+        return b - a;
+    }
+};
+
+function division(a, b) {
+    debugger;
+    return a / b;
+};
+
+function displayResult(resultMul, resultAdd, resultSub, resultDiv) {
     const resultElement = document.getElementById('result');
-    resultElement.textContent = `The result is: ${result}`;
+    resultElement.textContent = `Multiplication: ${resultMul}, Addition: ${resultAdd}, Subtraction: ${resultSub}, Division: ${resultDiv}`;
 };
